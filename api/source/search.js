6d0d6ca7bcff296d88712499f4179b425847db5f
chan <chanunlang@gmail.com>
2016-03-10 17:50:22 +0800

    0.0.4

--- api/source/search.js
+++ api/source/search.js
@@ -141,7 +141,7 @@ angular.module('search', [])
     var searchIndex = $q.defer();
     var results;
 
-    var worker = new Worker('js/search-worker.js');
+    var worker = new Worker('api/js/search-worker.js');
 
     // The worker will send us a message in two situations:
     // - when the index has been built, ready to run a query
